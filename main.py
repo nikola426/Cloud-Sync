6d0bc7a00d3da9d5c20ef2abf5787bc07af2acac
grayniks <nikola426@mail.ru>
2024-11-19 19:10:36 +0300

    Изменил логику сверки словарей внутри функции revise. Вынес в отдельную функцию подготовительную работу перед запуском цикла внутри функции main, а также сам цикл.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from loguru import logger
 
-import os
 from time import sleep
 
 from interface import YandexSyncInterface
@@ -8,33 +7,40 @@ import monitor
 import config
 
 
-def revise(local_dict, cloud_dict, inter):
+def revise(local_dict, cloud_dict, inter_inst):
     for file_name in local_dict.keys():
-        if md5 := cloud_dict.get(file_name) is None:
-            inter.load(file_name)
-        elif md5 != local_dict(file_name):
-            inter.reload(file_name)
+        if file_name not in cloud_dict:
+            inter_inst.load(file_name)
+        elif local_dict[file_name] != cloud_dict[file_name]:
+            inter_inst.reload(file_name)
 
     for file_name in cloud_dict.keys():
-        if local_dict.get(file_name) is None:
-            inter.delete(file_name)
+        if file_name not in local_dict:
+            inter_inst.delete(file_name)
 
+
+def infinite(sync_folder, inter_inst, period):
+    while True:
+        local_files_dict = monitor.scan(sync_folder)
+        cloud_files_dict = inter_inst.get_info()
+        if {(cloud_files_dict is not None) and
+            (local_files_dict is not None)}:
+            revise(local_files_dict, cloud_files_dict, inter_inst)
+        sleep(period)
+
+
+def data_preparation(sync_folder, period, log_file_path, interface):
+    logger.add(log_file_path, rotation='1 MB', compression='zip')
+    logger.info(f'Программа начала работу. Синхронизируемая папка: {sync_folder}')
+    inter_inst = interface()
+    period = int(period)
+
+    return sync_folder, inter_inst, period
 
 @logger.catch
 def main(module):
     if module == 'Yandex Disk':
-        logger.add(config.YA_LOG_FILE_PATH, rotation='1 MB', compression='zip')
-        logger.info('Программа начала работу.')
-        os.chdir(config.YANDEX_SYNC_FOLDER)
-        ya_inter = YandexSyncInterface()
-        period = int(config.YANDEX_PERIOD)
-
-        while True:
-            local_files_dict = monitor.scan()
-            cloud_files_dict = ya_inter.get_info()
-            if cloud_files_dict is not None:
-                revise(local_files_dict, cloud_files_dict, ya_inter)
-            sleep(period)
-            continue
+        infinite(*data_preparation(config.YANDEX_SYNC_FOLDER, config.YANDEX_PERIOD,
+                                   config.YA_LOG_FILE_PATH, YandexSyncInterface))
 
 main('Yandex Disk')
