1bd0b8ef2122f07f6e7641b0d9b63ddd65d24bcd
grayniks <nikola426@mail.ru>
2024-11-20 18:28:46 +0300

    Исправлена ошибка в monitor.py и main.py. Функция scan() начала принимать путь к синхронизируемой папке. Поменял местами загрузку файлов на сервер и удаление файлов с сервера.

--- main.py
+++ main.py
@@ -8,23 +8,22 @@ import config
 
 
 def revise(local_dict, cloud_dict, inter_inst):
+    for file_name in cloud_dict.keys():
+        if file_name not in local_dict:
+            inter_inst.delete(file_name)
+
     for file_name in local_dict.keys():
         if file_name not in cloud_dict:
             inter_inst.load(file_name)
         elif local_dict[file_name] != cloud_dict[file_name]:
             inter_inst.reload(file_name)
 
-    for file_name in cloud_dict.keys():
-        if file_name not in local_dict:
-            inter_inst.delete(file_name)
-
 
 def infinite(sync_folder, inter_inst, period):
     while True:
         local_files_dict = monitor.scan(sync_folder)
         cloud_files_dict = inter_inst.get_info()
-        if {(cloud_files_dict is not None) and
-            (local_files_dict is not None)}:
+        if (cloud_files_dict is not None) and (local_files_dict is not None):
             revise(local_files_dict, cloud_files_dict, inter_inst)
         sleep(period)
 
--- monitor.py
+++ monitor.py
@@ -4,12 +4,15 @@ import os
 import hashlib
 
 
-def calculate_md5(filepath):
+def calculate_md5(sync_folder, file_name):
     # Создаем объект MD5
     md5_hash = hashlib.md5()
 
+    #Объединяем путь к папке с именем файла
+    local_file_path = os.path.join(sync_folder, file_name)
+
     # Открываем файл в бинарном режиме
-    with open(filepath, "rb") as file:
+    with open(local_file_path, "rb") as file:
 
         # Читаем файл по частям для экономии памяти
         for chunk in iter(lambda: file.read(4096), b""):
@@ -19,11 +22,11 @@ def calculate_md5(filepath):
     return md5_hash.hexdigest()
 
 
-def scan():
+def scan(sync_folder):
     try:
-        files_dict = {file_name: calculate_md5(file_name) for file_name in os.listdir()}
+        files_dict = {file_name: calculate_md5(sync_folder, file_name) for file_name in os.listdir(sync_folder)}
     except Exception as e:
         logger.error(f'При попытке отсканировать локальную папку возникло исключение: {e}')
     else:
-        logger.info('Локальная папка успешно отсканирована.')
+        logger.info('Синхронизируемая папка успешно отсканирована.')
         return files_dict
